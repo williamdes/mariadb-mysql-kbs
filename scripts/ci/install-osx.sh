b00a8b09496f193c3289bf6f3c2ece4cd3a95967
William Desportes <williamdes@wdes.fr>
2019-03-20 20:25:34 +0100

    fix: osx CI
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- scripts/ci/install-osx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir ~/.homebrew_logs
-export HOMEBREW_LOGS="~/.homebrew_logs"
-export HOMEBREW_TEMP="/tmp"
-export HOMEBREW_INSTALL_BADGE="🌻"
-brew update
-brew install openldap libiconv php@7.2 composer
-sed -i -e 's/^memory_limit = .*/memory_limit = -1/' /usr/local/etc/php/7.2/php.ini

14d2a95d61e0c1831e82d0a8d2c67132c18de4d8
William Desportes <williamdes@wdes.fr>
2019-10-26 17:59:29 +0200

    feat: add tests for MySQL parser

--- test/index.js
+++ test/index.js
@@ -3,8 +3,10 @@
 process.env.TZ = 'UTC';
 const templates = require(__dirname + '/templates');
 const cleaner = require(__dirname + '/cleaner');
+const parser = require(__dirname + '/parser');
 
 suite('MariaDB MySQL KBS', function() {
     templates();
     cleaner();
+    parser();
 });

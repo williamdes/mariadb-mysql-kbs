5c269be50fe6bd97ffe67712ce172e26ccc7f519
William Desportes <williamdes@wdes.fr>
2019-12-08 21:08:27 +0100

    feat: Improve apidoc generation script
    [changelog skip]

--- scripts/apidoc-generate.sh
+++ scripts/apidoc-generate.sh
@@ -2,7 +2,11 @@
 cd $(dirname $0)/../scripts
 echo "Running in : $(pwd)"
 
+echo "Installing..."
 composer require --no-interaction sami/sami --prefer-dist --no-progress --no-suggest
+echo "Parsing..."
+./vendor/bin/sami.php --no-interaction parse ./sami-config.php
+echo "Updating..."
 ./vendor/bin/sami.php --no-interaction --verbose update ./sami-config.php
 ERR="$?"
 sleep 2

8c659616e3a7fe7c264e234bcc983b06de9a5585
William Desportes <williamdes@wdes.fr>
2019-12-08 15:13:11 +0100

    feat: Add script to trigger a workflow

---
+++ scripts/ci/trigger-cron.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ME=$(dirname $0)
+
+# Load up .env
+set -o allexport
+[[ -f $ME/.env ]] && source $ME/.env
+set +o allexport
+
+if [ -z "${GITHUB_TOKEN}" ]; then
+    echo "GitHub token missing"
+    exit 1
+fi
+
+DISPATCH_NAME="run-cron"
+
+curl --request POST \
+    --url https://api.github.com/repos/williamdes/mariadb-mysql-kbs/dispatches \
+    --header 'accept: application/vnd.github.everest-preview+json' \
+    --header 'authorization: token '${GITHUB_TOKEN}'' \
+    --header 'content-type: application/json' \
+    --data '{ "event_type": "'${DISPATCH_NAME}'" }'

44d115ed3d27355b75f3d6e081411c18003f0049
William Desportes <williamdes@wdes.fr>
2019-12-08 20:46:56 +0100

    ci: add --no-interaction and fix docs build

--- scripts/apidoc-generate.sh
+++ scripts/apidoc-generate.sh
@@ -2,5 +2,5 @@
 cd $(dirname $0)/../scripts
 echo "Running in : $(pwd)"
 
-composer require --no-interaction sami/sami --prefer-dist
+composer require --no-interaction sami/sami --prefer-dist --no-progress --no-suggest
 ./vendor/bin/sami.php --no-interaction --verbose update ./sami-config.php

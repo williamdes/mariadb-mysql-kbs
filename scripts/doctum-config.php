c9bcc1cacde7f2932e1015499b4fc277f6621e62
William Desportes <williamdes@wdes.fr>
2020-12-20 14:33:24 +0100

    feat: improve API docs config

--- scripts/doctum-config.php
+++ scripts/doctum-config.php
@@ -4,9 +4,13 @@ declare(strict_types = 1);
  * This file has been generated for code-lts/doctum
  */
 use Doctum\Doctum;
+use Doctum\RemoteRepository\GitHubRemoteRepository;
 use Symfony\Component\Finder\Finder;
 
-$iterator = Finder::create()->files()->name('*.php')->in(__DIR__ . '/../src');
+$srcFiles = realpath(__DIR__ . '/../src');
+$rootDir = realpath(__DIR__ . '/../') . DIRECTORY_SEPARATOR;
+
+$iterator = Finder::create()->files()->name('*.php')->in($srcFiles);
 
 $description = json_decode(file_get_contents(__DIR__ . '/../composer.json'))->description;
 
@@ -14,6 +18,16 @@ return new Doctum(
     $iterator, [
         'title'       => $description,
         'build_dir'   => __DIR__ . '/../docs/build',
-        'cache_dir'   => __DIR__ . '/../tmp'
+        'cache_dir'   => __DIR__ . '/../tmp',
+        'source_dir'  => $rootDir,
+        'remote_repository'    => new GitHubRemoteRepository('williamdes/mariadb-mysql-kbs', $rootDir),
+        'footer_link'          => [
+            'href' => 'https://github.com/williamdes/mariadb-mysql-kbs#readme',
+            'target'      => '_blank',
+            'rel'         => 'noopener',
+            'before_text' => 'Go back to the',
+            'link_text'   => 'MariaDB and MySQL Knowledge bases',
+            'after_text'  => 'project',
+        ]
     ]
 );
